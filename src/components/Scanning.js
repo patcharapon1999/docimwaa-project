f3f270f29fcb49177e7766355aa21a788e9d4525
Patcharapon  Kongchorb <patcharapon_k@kkumail.com>
2019-10-13 22:33:24 +0700

    test link page

--- src/components/Scanning.js
+++ src/components/Scanning.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
 import { Button, Jumbotron, Container, Collapse } from "react-bootstrap";
 import "../styles/Scanning.css";
-
-
+import { Link } from "react-router-dom";
 
 export default class Scanning extends Component {
-
   render() {
     return (
       <div>
@@ -14,9 +12,24 @@ export default class Scanning extends Component {
             <h1 className="header-name">Scanning</h1>
           </div>
         </Jumbotron>
-
         <div id="grad-line"></div>
-        
+
+        <div class="row">
+          <div class="columnL">
+            <h2>Column 1</h2>
+            <p>Some text..</p>
+          </div>
+          <div class="columnR">
+            <h2>Column 2</h2>
+            <p>Some text..</p>
+            <Link to="/optional">
+              <Button className="login-btn" type="submit">
+                Submit
+              </Button>
+            </Link>
+          </div>
+        </div>
+
       </div>
     );
   }
